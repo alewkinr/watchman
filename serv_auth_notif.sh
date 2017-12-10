7ed24b5ca704d6a9d0c918bfd866cce87b0c2a55
alewkinr <alewkin@icloud.com>
2017-12-10 23:22:15 +0300

    Добавлена удобная работа с hook url в слаке

---
+++ serv_auth_notif.sh
@@ -0,0 +1,12 @@
+### Уведомление об авторизации на сервере
+
+#slack hook url
+SLACK_URL="https://hooks.slack.com/services/XXXXXXXXX/XXXXXXXXX/XXXXXXXXXXXXXXXXXXXXXXXX"
+# Получаем ip адрес
+USER_IP=$(echo $SSH_CLIENT | awk '{ print $1}')
+
+# Формируем сообщение для отправки
+BOT_MESSAGE="Server login on $(whoami) from $USER_IP"
+
+# Отправляем сообщение в чат:
+curl -X POST --data-urlencode "payload={\"text\": \"$BOT_MESSAGE\"}" $SLACK_URL
